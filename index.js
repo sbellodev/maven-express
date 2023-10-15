e4596b9e854e5fff8bb3a8b6bf012ead67adc7a4
Santi Bello <sbellodev@gmail.com>
2023-10-15 14:36:59 +0200

    add helmet

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Middleware for parsing JSON data
+const helmet = require('helmet');
 
 // Apply the CORS middleware before defining routes
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", 'data:'],
+      // Add more directives as needed
+    },
+  })
+);
 const allowedOrigins = ['http://localhost:3000', 'https://santibello.es', 'https://santibello.es/michiapp'];
 app.use(cors({
   origin: allowedOrigins,
 }));
+
+
 
 // Use the user routes
 app.use(express.json());
